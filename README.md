b37f5e43909ea097ff02f15d77ced00ffb160243
keolas_s <keolas_s@epitech.eu>
2014-04-17 10:26:55 +0200

    Done

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 nettek
 ======
+
+
+ks_home ==> Pour le net chez soi.
+
+ks_school ==> Pour le net à l'école.
---
+++ install.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+sudo cp ./ks_home /bin
+sudo chmod 777 /bin/ks_home
+sudo chmod +x /bin/ks_home
+
+sudo cp ./ks_school /bin
+sudo chmod 777 /bin/ks_school
+sudo chmod +x /bin/ks_school
+
+sudo cp ./resolv_home /etc
+sudo cp ./resolv_school /etc
